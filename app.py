67b1138e9fe2211cc9e4aa86c688a5919af100f1
Theresa Inzerillo <theresa.inzerillo@gmail.com>
2016-07-03 20:45:00 -0400

    throws 404 error if invalid route name

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import constants
 
 app = Flask(__name__)
 
+valid_routes = [constants.RED_LINE, constants.GREEN_LINE, constants.ORANGE_LINE, constants.BLUE_LINE]
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
@@ -27,15 +28,19 @@ def displayAll():
 	
 @app.route("/get<string:route>Trains", methods=['GET'])
 def getTrainsOnRoute(route):
-	trains = [
-	{"id": route +"-train1", "longitude": 71.0589, "latitude": 42.3601, "route": route},
-	{"id": route +"-train2", "longitude": 71.0589, "latitude": 42.3601, "route": route},
-	{"id": route +"-train3", "longitude": 71.0589, "latitude": 42.3601, "route": route}
-	] # replace with array of dicts for given route from backend
-	
-	js = json.dumps(trains)
 
-	resp = Response(js, status=200, mimetype='application/json')
+	if route.lower() not in map(lambda x:x.lower(),valid_routes):
+		resp = Response(None, status=404, mimetype='application/json')
+	else:
+		trains = [
+			{"id": route +"-train1", "longitude": 71.0589, "latitude": 42.3601, "route": route},
+			{"id": route +"-train2", "longitude": 71.0589, "latitude": 42.3601, "route": route},
+			{"id": route +"-train3", "longitude": 71.0589, "latitude": 42.3601, "route": route}
+		] # replace with array of dicts for given route from backend
+	
+		js = json.dumps(trains)
+
+		resp = Response(js, status=200, mimetype='application/json')
 
 	return resp
 
