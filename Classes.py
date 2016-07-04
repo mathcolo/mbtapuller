95be8a9370a87d90ca6afc97d58d811dfd4e9beb
Preston Mueller <skierjunk@gmail.com>
2016-07-03 22:39:46 -0400

    Fix Classes.py

--- Classes.py
+++ Classes.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey, Date, DateTime
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, Date, DateTime, desc
 from sqlalchemy.ext.declarative import declarative_base
 from geopy.distance import vincenty
+from constants import *
 
 Base = declarative_base()
 
@@ -18,21 +19,39 @@ class Station(Base):
 class Trip(Base):
     __tablename__ = 'trips'
 
-    id = Column(String, primary_key=True)
-    date = Column(Date, primary_key=True)
+    id = Column(Integer, primary_key=True)
+
+    api_id = Column(String)
+    date = Column(Date)
 
     origin_station_id = Column(Integer, ForeignKey("stations.id"))
     destination_station_id = Column(Integer, ForeignKey("stations.id"))
 
     def get_direction(self):
         """
-        :return: Returns the trip direction as ±1, from a database perspective.
+        :return: Returns the trip direction as positive or negative 1, from a database perspective.
         """
         direction = self.destination_station_id > self.origin_station_id
         if direction:
             return 1
         else:
             return -1
+
+    def get_last_station(self, session):
+
+        all_trip_records = session.query(TripRecord).filter(TripRecord.trip_id.is_(self.id)).order_by(desc(TripRecord.stamp)).all()
+
+        potential_station = None
+        print "all_trip_records: %s" % all_trip_records
+        for trip_record in all_trip_records:
+            print "checking trip record %s" % trip_record.trip_id
+            potential_station = trip_record.get_exact_station(session)
+            print "potential_station human readable name: %s" % potential_station.name_human_readable
+            if potential_station:
+                return potential_station
+
+        assert potential_station is not None
+        return potential_station
 
 
 class TripRecord(Base):
@@ -45,7 +64,7 @@ class TripRecord(Base):
     location_lat = Column(Float)
     location_lng = Column(Float)
 
-    def _get_exact_station(self, session):
+    def get_exact_station(self, session):
 
         all_stations = session.query(Station).all()
         for station in all_stations:
@@ -60,16 +79,21 @@ class TripRecord(Base):
     def get_status(self, session):
 
         #direction is either positive or negative
-        associated_trip = session.query(Trip).filter(Trip.id.is_(self.trip_id))
+        associated_trip = session.query(Trip).filter(Trip.id.is_(self.trip_id)).first()
 
-        exact_station = self._get_exact_station(session)
+        exact_station = self.get_exact_station(session)
         if exact_station:
-            #return ()
-            pass
+            return STATUS_AT_STATION, exact_station
+
         # If we get this far, we're not at a station
+        # Find the next station
 
+        last_station = associated_trip.get_last_station(session)
 
-        # (status, station)
+        next_station_in_theory_id = last_station.id + self.get_direction()
+        my_route = session.query(Station).filter(Station.id.is_(self.origin_station_id)).first()
+        next_station_in_theory = session.query(Station).filter(Station.id.is_(next_station_in_theory_id)).first()
+        if my_route.name_api != next_station_in_theory.name_api:
+            return None
 
-
-        pass
+        return STATUS_IN_TRANSIT, next_station_in_theory
