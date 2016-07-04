0e241194a36b46129315c158ceff03792f6e6bba
Theresa Inzerillo <theresa.inzerillo@gmail.com>
2016-07-03 23:25:47 -0400

    added API

--- Functions.py
+++ Functions.py
@@ -1,18 +1,8 @@
 from geopy.distance import vincenty
 from Classes import Station
 import constants
-import pdb
-import argparse
-from Classes import *
-import APIFunctions
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine, or_
-
-db = create_engine('sqlite:///mbta.db', echo=False)
-Base.metadata.create_all(db)
-Session = sessionmaker(bind=db)
-session = Session()
-
 
 def current_station(trip_record, session):
     """
@@ -39,33 +29,34 @@ def get_stations(route, session):
 	if route.lower() == constants.RED_LINE.lower():
 	
 		start_id = session.query(Station).filter(Station.name_api == 'place-alfcl').one().id
-		
-		print(start_id)
 	
-		braintree = session.query(Station).filter(Station.route == route.title()).filter(or_(Station.id > start_id + 17, Station.id < start_id + 13)).order_by(Station.id).all()
+		braintree = session.query(Station).filter(Station.route == route.title()).filter(or_(Station.id > start_id + 16, Station.id < start_id + 13)).order_by(Station.id).all()
 		ashmont = session.query(Station).filter(Station.route == route.title()).filter(Station.id < start_id + 17).all()
+
+		braintree_names = [b.name_human_readable for b in braintree]
+		ashmont_names = [a.name_human_readable for a in ashmont]
 		
-		for b in braintree:
-			print b.name_human_readable
-		
-		return [braintree, ashmont]
+		return [braintree_names, ashmont_names]
 		
 	elif route.lower() == constants.GREEN_LINE.lower():
 
-		b = session.query(Station).filter(Station.route == constants.GREEN_LINE_B).all()
-		c = session.query(Station).filter(Station.route == constants.GREEN_LINE_C).all()
-		d = session.query(Station).filter(Station.route == constants.GREEN_LINE_D).all()
-		e = session.query(Station).filter(Station.route == constants.GREEN_LINE_E).all()
+		b_line = session.query(Station).filter(Station.route == constants.GREEN_LINE_B).all()
+		c_line = session.query(Station).filter(Station.route == constants.GREEN_LINE_C).all()
+		d_line = session.query(Station).filter(Station.route == constants.GREEN_LINE_D).all()
+		e_line = session.query(Station).filter(Station.route == constants.GREEN_LINE_E).all()
 		
-		stations = [b, c, d, e]
+		b_names = [b.name_human_readable for b in b_line]
+		c_names = [c.name_human_readable for c in c_line]
+		d_names = [d.name_human_readable for d in d_line]
+		e_names = [e.name_human_readable for e in e_line]
+		
+		stations = [b_names, c_names, d_names, e_names]
 			
 		return stations
 	
 	elif route.lower() in [constants.ORANGE_LINE.lower(), constants.BLUE_LINE.lower()]:
 		stations = session.query(Station).filter(Station.route == route.title()).all()
 
-		return stations
+		return [s.name_human_readable for s in stations]
 		
 	return None
-	
-get_stations('red', session)
