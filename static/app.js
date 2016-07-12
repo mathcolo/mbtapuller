dfb67bce58052d2cf2e39d0e6c23c20bca5c485b
Theresa Inzerillo <theresa.inzerillo@gmail.com>
2016-07-11 22:32:27 -0400

    merging

--- static/app.js
+++ static/app.js
@@ -1,5 +1,49 @@
 var app = angular.module('mbtaApp', ['ngRoute', 'ngMaterial', 'ngMdIcons']);
 
+
+
+app.controller('appCtrl', function ($http, $mdSidenav, $location, $rootScope) {
+
+    $http.get('/routes')
+	.then(function successCallback(response) {
+        $rootScope.trainRoutes = response.data;
+        $rootScope.trainRoutes.sort(function(a,b) {return (a["name"] > b["name"]) ? 1 : ((b["name"] > a["name"]) ? -1 : 0);} );
+		}, function errorCallback(response) {
+	});
+
+  this.go = function (name) {
+	console.log(name);
+    $location.path("/trains/" + name);
+  };
+  
+  this.toggleSidenav = function(menuId) {
+    $mdSidenav(menuId).toggle();
+  };
+  
+});
+
+
+// configure our routes
+app.config(function($routeProvider) {
+	$routeProvider
+
+		.when('/trains/:route_name', {
+			templateUrl : 'static/partials/route.html',
+			controller  : 'routeController'
+		})
+		.when('/404', {
+			templateUrl : 'static/partials/404.html'
+		})
+		.when('/error', {
+			templateUrl : 'static/partials/error.html'
+		})
+		.otherwise({
+			 redirectTo: '/'
+		 });
+		
+});	
+
+
 app.factory('httpErrorResponseInterceptor', ['$q', '$location',
   function($q, $location) {
     return {
@@ -26,47 +70,6 @@ app.config(['$httpProvider',
     $httpProvider.interceptors.push('httpErrorResponseInterceptor');
   }
 ]);
-
-// configure our routes
-app.config(function($routeProvider) {
-	$routeProvider
-
-		.when('/trains/:route_name', {
-			templateUrl : 'static/partials/route.html',
-			controller  : 'routeController'
-		})
-		.when('/404', {
-			templateUrl : 'static/partials/404.html'
-		})
-		.when('/error', {
-			templateUrl : 'static/partials/error.html'
-		})
-		.otherwise({
-			 redirectTo: '/'
-		 });
-		
-});	
-
-app.controller('appCtrl', function ($http, $mdSidenav, $location, $rootScope) {
-
-    $http.get('/routes')
-	.then(function successCallback(response) {
-        $rootScope.trainRoutes = response.data;
-        $rootScope.trainRoutes.sort(function(a,b) {return (a["name"] > b["name"]) ? 1 : ((b["name"] > a["name"]) ? -1 : 0);} );
-		}, function errorCallback(response) {
-	});
-
-  this.go = function (name) {
-	console.log(name);
-    $location.path("/trains/" + name);
-  };
-  
-  this.toggleSidenav = function(menuId) {
-    $mdSidenav(menuId).toggle();
-  };
-  
-});
-
   
 
 
