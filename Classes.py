5d62a65925166f2b1bee3817a0f21b06634ba2f3
Preston Mueller <skierjunk@gmail.com>
2016-07-12 22:54:00 -0400

    MySQL requires String lengths

--- Classes.py
+++ Classes.py
@@ -9,7 +9,7 @@ Base = declarative_base()
 class Route(Base):
     __tablename__ = 'routes'
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    name = Column(String(30))
 
 
 class Station(Base):
@@ -17,8 +17,8 @@ class Station(Base):
     id = Column(Integer, primary_key=True)
 
     route_id = Column(Integer, ForeignKey("routes.id"))
-    name_human_readable = Column(String)
-    name_api = Column(String)
+    name_human_readable = Column(String(50))
+    name_api = Column(String(50))
     location_lat = Column(Float)
     location_lng = Column(Float)
 
@@ -37,7 +37,7 @@ class Trip(Base):
 
     id = Column(Integer, primary_key=True)
 
-    api_id = Column(String)
+    api_id = Column(String(25))
     date = Column(Date)
 
     origin_station_id = Column(Integer, ForeignKey("stations.id"))
@@ -84,7 +84,7 @@ class TripRecord(Base):
 
     id = Column(Integer, primary_key=True)
 
-    trip_id = Column(String, ForeignKey("trips.id"))
+    trip_id = Column(String(25), ForeignKey("trips.id"))
     stamp = Column(DateTime)
     location_lat = Column(Float)
     location_lng = Column(Float)
