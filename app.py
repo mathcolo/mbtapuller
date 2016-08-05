630bd55405485715fdd2f5e7ac4dcc1676c879d3
Theresa Inzerillo <theresa.inzerillo@gmail.com>
2016-08-05 17:24:00 -0400

    Feature/tinzerillo/predictions (#54)
    * backend for predictions
    
    * remove comments
    
    * some renaming
    
    * schema changes and more of the predictions apifunction
    
    * fixed station name issuesgit add APIFunctions.py utilities.py
    
    * Red line train pull fix
    
    * getting closer to working!
    
    * displays in UI!
    
    * predictions refreshing!
    
    * removed some comments
    
    * fixed display of train predictions and destination buttons. added all resources

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import Database
 import Functions
 import db_objects as db
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, desc, asc
 
 Database.wait_for_available()
 session = Database.connect()
@@ -18,7 +18,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-	lines = sorted(getAllRoutes(), key=lambda k: k['name']) 
+	lines = sorted(get_all_routes(), key=lambda k: k['name']) 
 	
 	return render_template('index.html', lines=lines)
 
@@ -67,11 +67,17 @@ def get_all_stations():
 	
 	return json.dumps(stations)
 
-	
 @app.route("/stations/<string:route_id>", methods=['GET'])
 def get_stations_on_route(route_id):
 	stations = Functions.get_stations(route_id, session)
 	return json.dumps(stations)
+
+@app.route("/stations/<string:route_id>/terminal", methods=['GET'])
+def get_terminal_stations(route_id):
+	first = session.query(db.Station).filter(db.Station.route_id == route_id).order_by(asc(db.Station.id)).limit(1).first()
+	last = session.query(db.Station).filter(db.Station.route_id == route_id).order_by(desc(db.Station.id)).limit(1).first()
+	
+	return json.dumps({'first': first.name_human_readable, 'last': last.name_human_readable})
 
 @app.route("/routes", methods=['GET'])
 def get_all_routes():
@@ -80,19 +86,65 @@ def get_all_routes():
 
 @app.route("/id", methods=['GET'])
 def get_id_for_route():
-	id = session.query(db.Route).filter(db.Route.name == name).one().id
+	id = session.query(db.Route).filter(db.Route.name == request.args['name']).first().id
 	return json.dumps(id)
-
 
 @app.route("/station/<string:station_id>", methods=['GET'])
 def get_station_details(station_id):
 	station = session.query(db.Station).filter(db.Station.id == station_id).one()
-
 	route_name = session.query(db.Route).filter(db.Route.id == station.route_id).one().name
-
-	stations_details = {'name': station.name_human_readable, 'route_name': route_name, 'id': station.id}
+	
+	# add in predictions for both directions
+	stations_details = {'name': station.name_human_readable, 
+						'route_name': route_name, 
+						'id': station.id}
+	
 	return json.dumps(stations_details)
 
+@app.route("/station/<string:station_id>/direction/<string:direction>/details", methods=['GET'])
+def get_directed_station_details(station_id, direction):
+	station = session.query(db.Station).filter(db.Station.id == station_id).one()
+	route_name = session.query(db.Route).filter(db.Route.id == station.route_id).one().name
+	
+	predictions = json.loads(get_next_service_for_station(station_id, direction))
+	
+	stations_details = {'name': station.name_human_readable, 
+						'route_name': route_name, 
+						'id': station.id,
+					   'pre_1': predictions['prediction1'],
+					   'pre_2': predictions['prediction2']}
+	
+	return json.dumps(stations_details)
+
+@app.route("/station/<string:station_id>/direction/<string:direction>/nextservice", methods=['GET'])
+def get_next_service_for_station(station_id, direction):
+	predictions = Functions.current_predictions(session, station_id)
+	
+	directed_predictions = []
+    
+	if (predictions is not None):
+		for prediction in predictions:
+			trip = session.query(db.Trip).filter(db.Trip.id == prediction.trip_id).first()
+			dir = int(trip.destination_station_id > trip.origin_station_id)
+            
+			if (int(direction) is dir):
+				directed_predictions.append(prediction)
+		
+		if len(directed_predictions) == 0:
+			return json.dumps({'prediction1': None, 'prediction2' : None})
+				
+		directed_predictions.sort(key=lambda x: x.seconds_away_from_stop)
+		
+		if len(directed_predictions) > 1:
+			next_two_pre = {'prediction1': directed_predictions[0].seconds_away_from_stop, 'prediction2' : directed_predictions[1].seconds_away_from_stop}
+			
+		else:
+			next_two_pre = {'prediction1': directed_predictions[0].seconds_away_from_stop, 'prediction2' : None}
+			
+		return json.dumps(next_two_pre)
+	else:
+		return json.dumps({'prediction1': None, 'prediction2' : None})
+    
 
 if __name__ == "__main__":
 	app.run()
--- static/controllers/routeController.js
+++ static/controllers/routeController.js
@@ -1,8 +1,10 @@
-app.controller('routeController', function ($scope, $routeParams, $localStorage, $http, FavoritesService) {
+app.controller('routeController', function ($scope, $routeParams, $localStorage, $http, FavoritesService, $interval) {
     'use strict';
+	// need general service file to hold conversion of seconds to minutes and seconds
 	
 	$scope.route = $routeParams.route_name;
 	$scope.trains = [];
+	$scope.stations = [];
 	$scope.destination = true;
 	
 	$scope.favoritesExist = FavoritesService.favoritesExist;
@@ -18,23 +20,35 @@ app.controller('routeController', function ($scope, $routeParams, $localStorage,
 		
 		$http.get('/stations/' + route_id)
 		.then(function successCallback(response) {
-				$scope.stations = response.data;
+				$scope.station_ids = response.data;
 				
-				var stations_length = $scope.stations.length;
-				
-				$scope.first = $scope.stations[0].name;
-				$scope.last = $scope.stations[stations_length - 1].name;
-				
+				angular.forEach($scope.station_ids, function(value, key){
+					 $http.get('/station/' + value + '/direction/' + ($scope.destination ? 1 : 0 ) + '/details')
+					 .then(function successfulCallback(response) {
+						 $scope.stations.push(response.data);
+					 }, function errorCallback(response) {
+					});
+				});
 			}, function errorCallback(response) {
 				
 		});
 	};
 	
+	$scope.getTerminalStations = function(route_id) {
+		$http.get('/stations/' + route_id + '/terminal')
+		.then(function successCallback(response) {
+				$scope.first = response.data['first'];
+				$scope.last = response.data['last'];
+			}, function errorCallback(response) {
+		});
+	}
+	
 	$scope.init = function() {
 		$http.get('/id?name=' + $scope.route)
 		.then(function successCallback(response) {
-				var route_id = parseInt(response.data);
-				$scope.getData(route_id);
+				$scope.route_id = parseInt(response.data);
+				$scope.getData($scope.route_id);
+				$scope.getTerminalStations($scope.route_id);
 			}, function errorCallback(response) {
 		});
 		
@@ -55,6 +69,7 @@ app.controller('routeController', function ($scope, $routeParams, $localStorage,
 		if (destination != $scope.destination) {
 			$scope.filterTrains(destination);
 			$scope.stations = $scope.stations.reverse();
+			$scope.refreshData();
 			$scope.destination = destination;
 		}
 	};
@@ -109,7 +124,48 @@ app.controller('routeController', function ($scope, $routeParams, $localStorage,
 		}
 	};
 	
+	$scope.refreshData = function() {
+		angular.forEach($scope.stations, function(value, key){
+			 $http.get('/station/' + value.id + '/direction/' + ($scope.destination ? 1 : 0 ) + '/nextservice')
+			 .then(function successfulCallback(response) {
+				 $scope.stations[key].pre_1 = response.data['prediction1'];
+				 $scope.stations[key].pre_2 = response.data['prediction2'];
+			 }, function errorCallback(response) {
+			});
+		});	
+		
+		$http.get('/trains/' + $scope.route_id)
+		.then(function successCallback(response) {
+				$scope.allTrains = response.data;
+				$scope.filterTrains($scope.destination);
+			}, function errorCallback(response) {
+		});
+	};
+	
+	$scope.predictionText = function(station_id) {
+		var str = "";
+		for (var i = 0; i<$scope.stations.length; i++) {
+			if ($scope.stations[i].id === station_id) {
+				if ($scope.stations[i].pre_2 === null) {
+					if ($scope.stations[i].pre_1 === null) {
+						 str = "There is no scheduled service to this station at this time.";
+						break;
+					}
+					else {
+						str = "Next service: " + $scope.stations[i].pre_1 + " seconds away";
+						break;
+					}
+				}
+				str = "Next service: " + $scope.stations[i].pre_1 + " seconds away  | " + $scope.stations[i].pre_2 + " seconds away";
+				break;
+			}
+		}
+		
+		return str;
+	}
+	
 	
 	$scope.init();
+	$interval($scope.refreshData, 60000);
 	
 });
