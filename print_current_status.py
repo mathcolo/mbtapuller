af381f0458ff87ee301677b0f2432a275be16ca3
Preston Mueller <skierjunk@gmail.com>
2018-01-29 22:59:53 -0500

    Remove unnecessary breakpoint

--- print_current_status.py
+++ print_current_status.py
@@ -4,8 +4,6 @@ import datetime
 
 session = Database.connect()
 
-import ipdb; ipdb.set_trace()
-
 past_bound = datetime.datetime.utcnow() - datetime.timedelta(hours=3)
 
 for trip in session.query(db.Trip).filter(db.Trip.date < past_bound):
