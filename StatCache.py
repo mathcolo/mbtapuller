090c485a6d6e0eb9b078ef5401ad0364f8c494d7
Preston Mueller <skierjunk@gmail.com>
2018-04-02 20:26:07 -0400

    Use moving average n=1 instead of n=2

--- StatCache.py
+++ StatCache.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def circular_store(redis, stat_name, stat_value):
-    stat_value = np.average(np.append(np.asfarray(redis.lrange(stat_name, 0, 2)), stat_value))
+    stat_value = np.average(np.append(np.asfarray(redis.lrange(stat_name, 0, 1)), stat_value))
 
     redis.lpush(stat_name, stat_value)
     redis.ltrim(stat_name, 0, 100)
